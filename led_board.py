f77054533f22f2797f1f2fae8f53dde9d69bea5b
aleksols <aleko@stud.ntnu.no>
2019-10-14 16:16:30 +0200

    add functionality to led_board.py

--- led_board.py
+++ led_board.py
@@ -1,13 +1,47 @@
+import RPI.GPIO as GPIO
+import keypad as kp
+
 class LEDboard:
 
-    def setup(self):
-        pass
+    def __init__(self):
+        self.pins = []
+        self.pin_led_states = []
+        self.set_pin(0, -1)
+        self.set_pin(1, -1)
+        self.set_pin(2, -1)
 
-    def light_led(self):
-        pass
+    def setup(self):
+        self.pins = [5, 6, 13]
+        self.pin_led_states = [
+            [1, 0, -1],  # A
+            [0, 1, -1],  # B
+            [-1, 1, 0],  # C
+            [-1, 0, 1],  # D
+            [1, -1, 0],  # E
+            [0, -1, 1]  # F
+        ]
+        GPIO.setmode(GPIO.BCM)
+
+    def set_pin(self, pin_index, pin_state):
+        if pin_state == -1:
+            GPIO.setup(self.pins[pin_index], GPIO.IN)
+        else:
+            GPIO.setup(self.pins[pin_index], GPIO.OUT)
+            GPIO.output(self.pins[pin_index], pin_state)
+
+    def light_led(self, led_number):
+        for pin_index, pin_state in enumerate(self.pin_led_states[led_number]):
+            self.set_pin(pin_index, pin_state)
 
     def flash_all_leds(self):
         pass
 
     def twinkle_all_leds(self):
         pass
+
+if __name__ == '__main__':
+    led = LEDboard()
+    keypad = kp.Keypad()
+    while True:
+        x = int(keypad.get_next_signal())
+        led.light_led(x)
